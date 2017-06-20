03f2b6ac1ef1e82037fd2497d43cf67dc23a16e1
luisrog <luisrdzovejerogomez@gmail.com>
2017-06-20 12:40:08 +0200

    seed working

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :establishment
-  has_many :reviews through: :appointments
+  has_many :reviews, through: :appointments
   validates :title, presence: true
   validates :price, presence: true
   validates :category,  presence: true, inclusion: { in: %w[free-time sports programming languages cooking enterteinment art other], allow_nil: false }
