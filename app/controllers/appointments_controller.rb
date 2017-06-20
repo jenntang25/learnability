8f32c3a038be35b12a577a51c83ad9cc2be6f664
felipe quintana <felipe.q1989@gmail.com>
2017-06-20 13:43:55 +0200

    controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,11 +1,19 @@
 class AppointmentsController < ApplicationController
   def new
+    @appointment = Appointment.new
+    @course = Course.find(params[:course_id])
+
   end
 
   def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user = current_user
+    @appointment.course = @course
   end
 
   def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
   end
 
   def update
@@ -13,4 +21,12 @@ class AppointmentsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def appointment_params
+    params.require(:Appointment).permit(:course_id, :user_id)
+  end
 end
+
+
