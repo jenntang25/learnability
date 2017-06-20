4a6ca400ae0920dabedf553af85096dfbc300d92
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-20 11:55:12 +0200

    added stuff

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -1,19 +1,45 @@
 class EstablishmentsController < ApplicationController
+
   def new
+    @establishment = Establishment.new
+
   end
 
   def show
+    @establishment = Establishment.find(params[:id])
+
   end
 
   def create
+    @establishment = Establishment.new(establishment_params)
+    @establishment.user = current_user
+    if @establishment.save
+      redirect_to establishments_path(@establishment)
+    else
+      render :new
+    end
+
   end
 
   def update
+    @establishment = Establishment.find(params[:id])
+    @establishment.update(establisment_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to establishment_path(@establishment)
+
   end
 
   def destroy
+    @establishment = Establishment.find(params[:id])
+    @establishment.destroy
+    redirect_to establishments_path
   end
 
   def edit
+        @establishment = Establishment.find(params[:id])
+
+
   end
+
 end
