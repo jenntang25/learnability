1eb06b8aa683e55232ab5bc329d9f3263d07e187
Alberto Aldana <albertoaldanar@hotmail.com>
2017-06-20 12:08:15 +0200

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,15 @@
 Rails.application.routes.draw do
-  get 'courses/index'
-
-  get 'courses/new'
-
-  get 'courses/show'
-
-  get 'courses/destroy'
-
-  get 'courses/update'
-
-  get 'courses/edit'
-
-  get 'appointments/new'
-
-  get 'appointments/create'
-
-  get 'appointments/destroy'
-
-  get 'appointments/update'
-
-  get 'appointments/edit'
-
-  get 'reviews/index'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'establishments/new'
-
-  get 'establishments/show'
-
-  get 'establishments/create'
-
-  get 'establishments/update'
-
-  get 'establishments/destroy'
-
-  get 'establishments/edit'
-
-  get 'users/show'
 
   devise_for :users
   root to: 'pages#home'
+
+  resources :users
+  resources :establishments,  except: :index
+  resources :courses
+
+  resources :appointments do
+    resources :reviews, only: [:index, :new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
