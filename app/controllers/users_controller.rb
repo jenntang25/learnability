ff38f0b9430fafaadafce85ad05e130a82005268
Alberto Aldana <albertoaldanar@hotmail.com>
2017-06-21 18:15:27 +0200

    create edit methods for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
     before_action :set_user, only: [:show]
+
+
+    def edit
+    end
+
+
+  def update
+  end
+
   def show
   end
+
+
   def set_user
       @user = User.find(params[:id])
     end
