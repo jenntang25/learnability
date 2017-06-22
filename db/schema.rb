1fedf863434f61126069968d1c60294cd7cbd442
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-22 12:08:29 +0200

    added name to establishments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621125832) do
+ActiveRecord::Schema.define(version: 20170622100556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170621125832) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
     t.index ["user_id"], name: "index_establishments_on_user_id", using: :btree
   end
 
