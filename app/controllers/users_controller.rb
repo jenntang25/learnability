fb1240ba7817f610cd6f1f55d889229665e80d4b
Alberto Aldana <albertoaldanar@hotmail.com>
2017-06-22 12:48:44 +0200

    edit user and profile ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-    before_action :set_user, only: [:show]
+  skip_before_action :authenticate_user!
 
 
  def edit
-
  end
 
 
   def update
+    current_user.update(params_user)
+    redirect_to user_path(current_user)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
 
-  def set_user
-      @user = User.find(params[:id])
-    end
+  def params_user
+    params.require(:user).permit!
+  end
 end
