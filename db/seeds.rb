980376ee0f8604053acc69a9804081753f8fbf7f
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-23 15:33:50 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,22 @@ Course.destroy_all
 Establishment.destroy_all
 User.destroy_all
 
-user1 = User.create!(email: "test@test.com", password: "123456")
-user2 = User.create!(email: "test1@test.com", password: "123456")
-user3 = User.create!(email: "test2@test.com", password: "123456")
+# user1 = User.create!(email: "test@test.com", password: "123456")
+# user2 = User.create!(email: "test1@test.com", password: "123456")
+# user3 = User.create!(email: "test2@test.com", password: "123456")
 
-est1 = Establishment.create!(country: "Spain", city: "Barcelona", user: user1)
-est2 = Establishment.create!(country: "Mexico", city: "Cancún", user: user2)
-est3 = Establishment.create!(country: "USA", city: "Santa Monica", user: user3)
+# est1 = Establishment.create!(country: "Spain", city: "Barcelona", user: user1)
+# est2 = Establishment.create!(country: "Mexico", city: "Cancún", user: user2)
+# est3 = Establishment.create!(country: "USA", city: "Santa Monica", user: user3)
 
-course1 = Course.create!(title: "English CPE", price: 50, category: "languages", establishment: est1)
-course2 = Course.create!(title: "French cuisine", price: 40, category: "cooking", establishment: est2)
-course3 = Course.create!(title: "Picasso's influence", price: 12, category: "art", establishment: est3)
+# course1 = Course.create!(title: "English CPE", price: 50, category: "languages", establishment: est1)
+# course2 = Course.create!(title: "French cuisine", price: 40, category: "cooking", establishment: est2)
+# course3 = Course.create!(title: "Picasso's influence", price: 12, category: "art", establishment: est3)
 
-app1 = Appointment.create!(date: "07/07/1985", user: user1, course: course1)
-app2 = Appointment.create!(date: "07/07/1986", user: user2, course: course2)
-app3 = Appointment.create!(date: "07/07/1987", user: user3, course: course3)
+# app1 = Appointment.create!(date: "07/07/1985", user: user1, course: course1)
+# app2 = Appointment.create!(date: "07/07/1986", user: user2, course: course2)
+# app3 = Appointment.create!(date: "07/07/1987", user: user3, course: course3)
 
-rev1 = Review.create!(appointment: app1, course: course1, stars: 3)
-rev2 = Review.create!(appointment: app2, course: course2, stars: 4)
-rev3 = Review.create!(appointment: app3, course: course3, stars: 5)
+# rev1 = Review.create!(appointment: app1, course: course1, stars: 3)
+# rev2 = Review.create!(appointment: app2, course: course2, stars: 4)
+# rev3 = Review.create!(appointment: app3, course: course3, stars: 5)
