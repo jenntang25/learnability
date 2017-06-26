0fb947701220740c03d06e45062bb1701d484425
felipe quintana <felipe.q1989@gmail.com>
2017-06-26 13:39:34 +0200

    geocoding

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -6,20 +6,21 @@ class Establishment < ApplicationRecord
 
   has_many :courses, dependent: :destroy
 
+  # validates :street, presence: true
   validates :country, presence: true
   validates :city, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :street_changed?
 
 
    multisearchable :against => [:name, :description]
 
-
-  def address
-    "#{self.street}, #{self.postal_code}, #{self.city}, #{self.country}"
+  def self.geocode_all
+    self.all.map { |model| model.geocode && model.save }
   end
 
-
-
-
+  def address
+    [self.street, self.postal_code, self.city, self.country].select{|i| i}.join(", ")
+  end
 end
