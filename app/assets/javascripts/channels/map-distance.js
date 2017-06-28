41025afba238b0682e8c3915a82513e5dc6b7c7d
Alberto Aldana <albertoaldanar@hotmail.com>
2017-06-28 16:03:04 +0200

    save work

---
+++ app/assets/javascripts/channels/map-distance.js
@@ -0,0 +1,4 @@
+
+var distance = Geocoder::Calculations.distance_between([current_user.latitude, current_user.longitude],[user.establishment.latitude, user.establishment.longitude])
+
+var user_long= $("#user-long");
