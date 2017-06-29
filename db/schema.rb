74a2e308cad90bc939ff3a91039855d1baf5e740
felipe quintana <felipe.q1989@gmail.com>
2017-06-29 11:07:36 +0200

    tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623081842) do
+ActiveRecord::Schema.define(version: 20170628232112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170623081842) do
     t.text     "description"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "tag"
     t.index ["establishment_id"], name: "index_courses_on_establishment_id", using: :btree
   end
 
