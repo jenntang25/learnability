7de090921f91e4c995a23c8df97edc299ef3df5e
felipe quintana <felipe.q1989@gmail.com>
2017-06-29 17:06:20 +0200

    bien

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,20 @@
 class Course < ApplicationRecord
 
   include PgSearch
-
   CATEGORIES = %w(Education Technology Business Sports Music Cooking Health Art Photography Entertainment)
+
+belongs_to :establishment
+has_many :reviews, through: :appointments, dependent: :destroy
+has_many :appointments
+has_attachments :photos, maximum: 1
+validates :title, presence: true
+validates :price, presence: true
+validates :tag, presence: true, length: { maximum: 50 }
+ #validates :photos, presence: true
+ validates :category,  presence: true, inclusion: { in: Course::CATEGORIES, allow_nil: false }
+
+ multisearchable against: [:title, :category, :description]
+
 
   CATEGORIES_MAPPING  = {
    "Sports" => "fa fa-futbol-o",
@@ -18,21 +30,8 @@ class Course < ApplicationRecord
  }
 
 
-# def self.distance
-#   Geocoder::Calculations.distance_between([current_user.latitude,current_user.longitude],[self.latitude,self.longitude])
-# end
 
- belongs_to :establishment
- has_many :reviews, through: :appointments, dependent: :destroy
- has_many :appointments
- has_attachments :photos, maximum: 1
- validates :title, presence: true
- validates :price, presence: true
- validates :tag, presence: true, length: { maximum: 50 }
- #validates :photos, presence: true
- validates :category,  presence: true, inclusion: { in: Course::CATEGORIES, allow_nil: false }
 
- multisearchable against: [:title, :category, :description]
 
 
  def self.search(search)
